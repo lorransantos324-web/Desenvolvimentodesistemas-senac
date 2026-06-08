cea33c111580d0f85415ef8242cc0e3b1c1dbf6a
RicardoCode1 <carlosricardox12@gmail.com>
2026-06-08 11:29:17 -0300

    AULA_05

--- UC1/AULA_05/assets/js/scripts.js
+++ UC1/AULA_05/assets/js/scripts.js
@@ -1,2 +1,27 @@
- console.log("Retorno no console DEV da página")
-    console.log("lorran DEV 123")
+// VARIÁVEL
+let nome = "Lorran 25"
+nome = " Papai Noel " + nome
+nome = " Silva " + nome
+// CONCATENAR: JUNTAR USE +
+console.log("Nome:" + nome)
+
+const nomeDev = "Lorran"
+
+
+console.log("Uso constante: " + nomeDev)
+
+let nomeUser = "Lorran";
+let idade = "25";
+let ecivil = "Soltero" ;
+let totalfilho = 0
+let naturalidade = "Eusébio"
+let estado = "Ceará" 
+
+let dados = " Nome: " 
++ nomeUser + "\n Idade: " 
++ idade + "\n Estado civil "
++ ecivil + "\n Naturalidade "
++ naturalidade + "\n Estado "
++ estado;
+// \n + quebra de linha
+console.log(dados)
