17e64a3db1cc81ff71d3f490b5ac5a25c547951d
RicardoCode1 <carlosricardox12@gmail.com>
2026-06-10 11:31:47 -0300

    AULA_07

---
+++ UC1/AULA_07/assets/js/script.js
@@ -0,0 +1,31 @@
+// analise booleana
+let status = true
+let ação = prompt("indique uma ação 0 ou 1");
+
+
+if(ação!=="" && ação<=1  && ação>=0){
+let confimar =confirm("deseja alterar o sistema?");
+
+if(confimar){
+
+if(ação===0){
+    status = false
+   console.log(`O SISTEMA FOI DESATIVADO
+    O STATUS AGORA É ${status}`)
+    
+}else{(` O SISTEMA ESTÁ ATIVADO
+    O STATUS É ${status}`)
+}
+
+    console.log(`Voce confirmou a alteraçao${ação}`)
+
+
+} else {
+  alert ("você canncelou a alteração")
+
+}
+}
+
+ else { alert ("você nao informou o código de ação")
+
+}
